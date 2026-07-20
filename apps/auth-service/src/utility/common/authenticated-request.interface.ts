1e92f37a63f1905d37d478f11885fa36f33569e4
molo2206 <molomoloisraelpacifique@gmail.com>
2026-07-20 23:27:06 +0200

    Corrections

--- apps/auth-service/src/utility/common/authenticated-request.interface.ts
+++ apps/auth-service/src/utility/common/authenticated-request.interface.ts
@@ -1,46 +1,17 @@
-// apps/api-gateway/src/types/current-user.types.ts
-
 import { Request } from 'express';
-import { 
-  user_role, 
-  user, 
-  user_status, 
-  user_kycStatus,
-  user_merchantType,
-  user_passwordStatus 
-} from '@prisma/client';
+import { user_role, user, user_status } from '@prisma/client';
 
 export type CurrentUser = {
   id: string;
   email?: string | null;
   phone?: string | null;
   full_name?: string | null;
-  account_number?: string | null;
-  merchantCode?: string | null;
-  branch?: string | null;
   role: user_role;
   status: user_status;
   deleted?: boolean | null;
+  countryCode?: string | null;
   createdAt: Date;
   updatedAt: Date;
-  pin?: string | null;
-  passwordStatus?: user_passwordStatus | null;
-  fcmToken?: string | null;
-  pinstatus?: boolean | null;
-  businessName?: string | null;
-  failed_login_attempts?: number | null;
-  locked_until?: Date | null;
-  failed_pin_attempts?: number | null;
-  pin_locked_until?: Date | null;
-  kycStatus?: user_kycStatus | null;
-  countryCode?: string | null;
-  profileImage?: string | null;
-  merchantType?: user_merchantType | null;
-  businessCategory?: string | null;
-  businessAddress?: string | null;
-  maintenance_fee?: number | null;
-  last_maintenance_date?: Date | null;
-  is_maintenance_exempt?: boolean | null;
 };
 
 export interface AuthenticatedRequest extends Request {
